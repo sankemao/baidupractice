dbf13391d022ab83c2db6cf0a32ea7331f6db28a
jin <210980059@qq.com>
2018-10-09 17:44:05 +0800

    js

--- day31-41/datasource.js
+++ day31-41/datasource.js
@@ -34,4 +34,11 @@ let sourceData = [{
     product: "智能音箱",
     region: "华南",
     sale: [10, 40, 10, 6, 5, 6, 8, 6, 6, 6, 7, 26]
-}]
+}];
+
+//根据checkbox勾选过滤数据
+function getData() {
+    return sourceData.filter((item, index, self) => {
+        return true;
+    });
+}
