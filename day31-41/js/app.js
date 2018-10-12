5dd00a925c29bba23b7e64609c6965d53f36207f
jin <210980059@qq.com>
2018-10-12 17:31:49 +0800

    折线图完成

--- day31-41/js/app.js
+++ day31-41/js/app.js
@@ -2,6 +2,8 @@ let areaWrapper = document.querySelector('#area-wrapper');
 let goodsWrapper = document.querySelector('#goods-wrapper');
 let selectForm = document.querySelector('.select-form');
 let tableWrapper = document.querySelector('#table-wrapper');
+let barWrapper = document.querySelector('#bar-svg');
+let lineWrapper = document.querySelector('#line-canvas');
 
 //处理CheckBox
 handleCheckbox(areaWrapper);
@@ -20,15 +22,19 @@ renderTable(tableWrapper, selectedData);
 //开始处理图表
 tableWrapper.onmouseover = (e) => {
     let target = e.target;
-    console.log(target);
     if (target && target.nodeName.toUpperCase() == 'TD') {
         let parent = target.parentNode;
         if (!Number(parent.childNodes[3].innerText)) return;
         let data = [];
         Array.prototype.slice.call(parent.childNodes, 0).forEach(child => {
-            data.push(child.innerText);
+            let numData = Number(child.innerText);
+            if(numData){
+                data.push(numData);
+            }
         });
-        // console.log('当前选中的数组为：' + data);
-        
+
+        //绘制柱状图
+        drawBar(barWrapper, data);
+        drawLine(lineWrapper, data);
     }
 };
--- day31-41/js/bar.js
+++ day31-41/js/bar.js
@@ -1,6 +1,34 @@
 /**
  * 柱状图
  */
-function drawBar(canvas, data){
+function drawBar(svg, data) {
+    const padding = 25;
+    const width = 700;
+    const height = 300;
+    const axisWidth = width - padding;
+    const axisHeight = height - padding;
+    const axisColor = '#000';
+    const barGap = 12;
+    const barWidth = (width - barGap * (data.length + 1) - 2 * padding) / data.length;
+
+    //设置svg尺寸
+    svg.setAttribute('width', width);
+    svg.setAttribute('height', height);
+
+    //绘制坐标轴
+    let barHtml = [];
+    barHtml.push("<line x1=" + padding + " y1=0 x2=" + padding + " y2=" + axisHeight + " stroke=" + axisColor + " stroke-width='2'/>");
+    barHtml.push("<line x1=" + padding + " y1=" + axisHeight + " x2=" + axisWidth + " y2=" + axisHeight + " stroke=" + axisColor + " stroke-width='2'/>");
+
+    //绘制柱子
+    let dataMax = Math.max(...data);
+    let rate = dataMax / (axisHeight-padding);
     
+    let x = padding + barGap;
+    data.forEach(element => {
+        barHtml.push('<rect width=' + barWidth + ' height=' + (element / rate) + ' x=' + x + ' y=' + (axisHeight - element / rate) + ' fill=#ff0000' +  ' />')
+        x += barWidth + barGap;
+    });
+
+    svg.innerHTML = barHtml.join('');
 }
